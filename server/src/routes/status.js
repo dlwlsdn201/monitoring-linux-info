6304993e1419dfcb94a1b1123426111a68e061fb
JinWoo_Lee <dlwlsdn201@naver.com>
2024-05-19 16:28:56 +0900

    :fire: 불필요한 console.log 삭제

--- server/src/routes/status.js
+++ server/src/routes/status.js
@@ -4,14 +4,12 @@ import { requestDiskStatusAPI, requestCpuStatusAPI } from "../api/status";
 const router = Router();
 // 서버의 CPU 사용 현황 제공 API
 router.get("/cpu", async (req, res) => {
-  console.group("requested by:", req.url);
   try {
     const result = await requestCpuStatusAPI();
     console.log("🟢 return data:", result);
     console.groupEnd();
     return res.status(200).json(result);
   } catch (error) {
-    console.groupEnd();
     return res.status(500).json({ error: error.message });
   }
 });
@@ -25,7 +23,6 @@ router.get("/disk", async (req, res) => {
     return res.status(200).json(result);
   } catch (error) {
     console.log({ error });
-    console.groupEnd();
     return res.status(500).json({ error: error.message });
   }
 });
